653c34f9dcd14d2d9a899ed0912ceeedc719ee99
bshivudu <90346452+bshivudu@users.noreply.github.com>
2021-09-08 11:01:53 -0700

    Create math.js with basic functions

---
+++ BasicJest/math.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    console.log('The input values to the sum function: ' + a + ', ' + b);
+    return a + b;
+}
+
+function multiply(a, b) {
+    console.log('The input values to the sum function: ' + a + ', ' + b);
+    return a * b;
+}
+module.exports = {sum, multiply};
